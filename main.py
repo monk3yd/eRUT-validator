4107435fed6122bd31f554146c816ff9b778076f
monk3yd <jegajardog@gmail.com>
2022-05-06 19:32:31 -0400

    Convert jpeg to png working.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import base64
+import pyqrcode
 
+from PIL import Image
 from validator import EValidator
 
 
 def main():
     # rut_personal = 
-    rut_empresa = "76/745.604-2"
-    
-    # Load image .png .jpg or .jpeg
-    with open("image.jpeg", "rb") as image_file:
-        # Convert image to base64img string
-        encoded_image = base64.b64encode(image_file.read())
+    rut_empresa = "76.745.604-2"
 
-    validator = EValidator(base64img=encoded_image)
+    img = Image.open(r"image.jpeg")
+    img.save(r'image.png')
+
+    # qr = pyqrcode.create("image.png")
+    # image_as_str = qr.png_as_base64_str(scale=5)
+
+    # Load image .png .jpg or .jpeg
+    # with open("image.jpeg", "rb") as image_file:
+    #     # Convert image to base64img string
+    #     encoded_image = base64.b64encode(image_file.read())
+
+    # validator = EValidator(base64img=encoded_image)
 
 
 if __name__ == "__main__":
