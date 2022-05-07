0a2dacf868f391290b497af058132577cff88573
monk3yd <jegajardog@gmail.com>
2022-05-06 21:03:46 -0400

    Input base64img into EValidator object

--- main.py
+++ main.py
@@ -2,37 +2,29 @@ import base64
 import pyqrcode
 from pyzbar.pyzbar import decode, ZBarSymbol
 
-
 from PIL import Image
 from validator import EValidator
 
 
 def main():
-    # --- Input ---
+    # Convert .jpeg into .png image and save
+    # img = Image.open(r"image.jpeg")
+    # img.save(r'image.png')
 
+    # --- Input ---
     # RUT_PERSONAL = 
     RUT_EMPRESA = "76.745.604-2"
+    base64img = base64.b64encode(open("image.png", "rb").read())
 
-    # Convert .jpeg to .png
-    img = Image.open(r"image.jpeg")
-    img.save(r'image.png')
+    # --- Blackbox ---
+    validator = EValidator(str(base64img))
 
-    # Create qr code
-    # qr = pyqrcode.create("QR_Validator")
-    # qr.png("qr.png", scale=3, quiet_zone=4)
-    # image_as_str = qr.png_as_base64_str(scale=5)
-
-    # Extract qr code from img
-    # qr = qrtools.QR()
-    qr = decode(Image.open("image.png"), symbols=[ZBarSymbol.QRCODE])
-    print(qr)
-
+    
     # Load image .png .jpg or .jpeg
     # with open("image.jpeg", "rb") as image_file:
     #     # Convert image to base64img string
     #     encoded_image = base64.b64encode(image_file.read())
 
-    # validator = EValidator(base64img=encoded_image)
 
 
 if __name__ == "__main__":
--- validator.py
+++ validator.py
@@ -1,3 +1,12 @@
+from pprint import pprint
+
+
 class EValidator():
-    def __init__(base64img: str) -> bool:
-        print(base64img)
+    def __init__(self, base64img: bytes or str) -> bool:
+        pprint(base64img)
+        pprint(type(base64img))
+
+
+        # Extract qr code from img
+        # qr = decode(Image.open("image.png"), symbols=[ZBarSymbol.QRCODE])
+        # print(qr)  # Output list
